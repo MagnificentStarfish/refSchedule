2f652c4846061c196a0ab6f6766b897188c70a38
Donald Powell <Djp0242@gmail.com>
2023-12-25 14:58:44 -0800

    fixed mongodb connection

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/refSchedule', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/refSchedule')
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
