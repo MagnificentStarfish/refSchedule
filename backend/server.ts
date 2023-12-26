873f238538fb836a9f10f63596b22c11aecd2768
Donald Powell <Djp0242@gmail.com>
2023-12-26 14:41:01 -0800

    updated connection using Mongoose 6.0 syntax

--- backend/server.ts
+++ backend/server.ts
@@ -2,4 +2,4 @@ import mongoose from 'mongoose';
 
 mongoose.connect('mongodb://localhost:27017/refSchedule')
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+  .catch((err: Error) => console.error('Could not connect to MongoDB', err));
