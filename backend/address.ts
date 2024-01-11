1c72c4f8831496eb5e643385700b04cc7436d6fa
Donald Powell <Djp0242@gmail.com>
2024-01-11 13:53:50 -0800

    address added

---
+++ backend/address.ts
@@ -0,0 +1,19 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IAddress extends Document {
+  street: string;
+  city: string;
+  state: string;
+  zipCode: string;
+}
+
+const AddressSchema: Schema = new mongoose.Schema({
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zipCode: { type: String, required: true },
+});
+
+const Address = mongoose.model<IAddress>('Address', AddressSchema);
+
+export default Address;
