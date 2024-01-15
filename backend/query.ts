2f0304272911ea1997336a6eae6d1294b2dae556
Donald Powell <Djp0242@gmail.com>
2024-01-15 15:17:16 -0800

    all models created (for now)

---
+++ backend/query.ts
@@ -0,0 +1,13 @@
+import { IResolvers } from '@graphql-tools/utils';
+import Game from './game';
+
+const Query: IResolvers = {
+  games: async () => {
+    return await Game.find({});
+  },
+  game: async (_: any, { id }: { id: string }) => {
+    return await Game.findById(id);
+  },
+};
+
+export default Query;
