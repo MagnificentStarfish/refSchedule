594c6194d4111b49698f11bbd64b3b9751a4817d
Donald Powell <Djp0242@gmail.com>
2024-01-17 12:20:41 -0800

    server built, queries written, resolvers written

--- backend/graphqlServer.ts
+++ backend/graphqlServer.ts
@@ -1,16 +1,36 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
 const User = require('./user');
+const LocationModel = require('./location');
 
 
 const typeDefs = gql`
   type User {
     firstName: String
     lastName: String
+    email: String
+    address: Address
+    picture: String
+    maxTravelDistance: Int
+    proficiency: String
+    availability: [Availability]
+    games: [Game]
   }
 
   type Query {
     users: [User]
+  }
+
+  type Address {
+    street: String
+    city: String
+    state: String
+    zip: String
+  }
+
+  type Location {
+    name: String
+    address: Address
   }
 `;
 
@@ -19,6 +39,17 @@ const resolvers = {
   Query: {
     users: async () => {
       return await User.find();
+    },
+    locations: async () => {
+      return await LocationModel.find();
+    }
+  },
+  User: {
+    address: async (parent: any) => {
+      return await parent.populate('address').execPopulate();
+    },
+    games: async (parent: any) => {
+      return await parent.populate('games').execPopulate();
     },
   },
 };
