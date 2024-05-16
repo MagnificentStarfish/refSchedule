7f6802c6e692ce8c7b03933cdb2a60076e6eaf69
Donald Powell <Djp0242@gmail.com>
2024-05-16 10:52:48 -0700

    testing not interfacing well with typescript

---
+++ backend/__tests__/queryDatabase.test.ts
@@ -0,0 +1,24 @@
+import User from '../src/user';
+import mongoose from 'mongoose';
+
+
+describe('Database Connection', () => {
+  beforeAll(async () => {
+    await mongoose.connect('mongodb://localhost:27017/refSchedule', {
+    });
+  });
+
+  test('successfully connects to the database', async () => {
+    const db = mongoose.connection;
+    expect(db.readyState).toBe(1); // 1 means connected
+  });
+
+  test('can retrieve data from the database', async () => {
+    const users = await User.find({});
+    expect(users.length).toBeGreaterThan(0);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+});
